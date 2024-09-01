d9ed2d4590fe4e3991b940f0dedcbfd8f0f763a4
linetgitonga <linetgitonga55@gmail.com>
2024-09-01 00:29:58 +0000

    personal data

---
+++ 0x00-personal_data/README.md
@@ -0,0 +1,3 @@
+personal dta
+pii and piii
+
---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Author:Linet
+Defines a hash_password function to return a hashed password
+"""
+import bcrypt
+from bcrypt import hashpw
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Returns a hashed password
+     """
+    b = password.encode()
+    hashed = hashpw(b, bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check whether a password is valid
+    Args:
+        hashed_password (bytes): hashed password
+        password (str): password in string
+    Return:
+        bool
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
